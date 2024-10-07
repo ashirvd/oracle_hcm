c0e6d2c336919569b11d7b9226eda064e1e810c2
ashirvd <ashir.afzal@venturedive.com>
2024-10-07 14:48:41 +0500

    Add files via upload

---
+++ oracle_hcm_finance/pytests/test_ebit.py
@@ -0,0 +1,40 @@
+import pytest
+from oracle_hcm_finance.Finance_KPIs.YTD_EBIT_kpis import ebit
+
+@pytest.mark.parametrize("month, expected_output", [
+    (1, "Using data for the month: January"),
+    (2, "Using data for the month: February"),
+    (3, "Using data for the month: March"),
+    (4, "Using data for the month: April"),
+    (5, "Using data for the month: May"),
+    (6, "Using data for the month: June"),
+    (7, "Using data for the month: July"),
+    (8, "Using data for the month: August"),
+    (9, "Using data for the month: September"),
+], ids=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September"
+])
+@pytest.mark.run(order=1)
+def test_ebit(month, expected_output, capsys):
+    ebit(month)  # Call the ebit function
+    captured = capsys.readouterr()  # Capture the output
+    assert expected_output in captured.out  # Check if expected output is in captured stdout
+    # Check if the expected output is in the captured output
+
+
+    # Optionally, print all output for debugging purposes
+    print("Captured Output:")
+    print(captured.out)
+#pytest test_ebit.py --html=report_ebit.html --self-contained-html
+#pip install pytest-ordering
+
+#run this command to run all pytests in 'pytests' folder
+#pytest --html=all_reports.html
