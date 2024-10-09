4276bb088eebb3be3ea554545607fb955bb95d1f
ashirvd <ashir.afzal@venturedive.com>
2024-10-09 20:51:15 +0500

    Add files via upload

---
+++ oracle_hcm_finance/pytests_hcm/test_actual_vs_budget.py
@@ -0,0 +1,21 @@
+import pytest
+from oracle_hcm_finance.HCM_KPIs.actual_vs_budget import actual_vs_budget  # Adjust import as necessary
+
+@pytest.mark.parametrize("month", [1, 2, 3, 4, 5, 6, 7], ids=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July"
+])
+@pytest.mark.run(order=1)  # Adjust order as needed
+def test_actual_vs_budget(month, capsys):
+    actual_vs_budget(month)  # Call the actual_vs_budget function
+    captured = capsys.readouterr()  # Capture the output
+    assert "Percentage for the" in captured.out  # Check if expected output is in captured stdout
+
+    # Optionally, print all output for debugging purposes
+    print("Captured Output:")
+    print(captured.out)
---
+++ oracle_hcm_finance/pytests_hcm/test_staff_cost.py
@@ -0,0 +1,22 @@
+import pytest
+from oracle_hcm_finance.HCM_KPIs.staff_cost import staff_cost
+
+@pytest.mark.parametrize("month", [1, 2, 3, 4, 5, 6, 7,8], ids=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August"
+])
+@pytest.mark.run(order=6)
+def test_actual_vs_budget(month, capsys):
+    staff_cost(month)  # Call the actual_vs_budget function
+    captured = capsys.readouterr()  # Capture the output
+    assert "Processing file for month" in captured.out  # Check if expected output is in captured stdout
+
+    # Optionally, print all output for debugging purposes
+    print("Captured Output:")
+    print(captured.out)
